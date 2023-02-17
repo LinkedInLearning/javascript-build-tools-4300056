c721bc2e197fa26c0da319ffaa511dd60c3fc0b4
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2023-02-17 00:05:52 +0000

    add vite setup

--- example-vite/vite.config.js
+++ example-vite/vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   root: './src',
   publicDir: '../public',
   build: { outDir: '../dist' },
+  server: {
+    host: true,
+        port: 5173,
+    hmr: { clientPort: 5173 },
+  }
   plugins: [
     viteImagemin({
       optipng: {
